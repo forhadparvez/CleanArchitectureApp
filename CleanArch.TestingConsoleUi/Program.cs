be47215fcd33d3139f77776697d68c5f65eace3a
forhadparvez <forhad.parvez@yahoo.com>
2018-03-18 13:15:30 +0600

    Add Console Ui

--- CleanArch.TestingConsoleUi/Program.cs
+++ CleanArch.TestingConsoleUi/Program.cs
@@ -1,10 +1,20 @@
-﻿
+﻿using ClearArch.ServiceLibrary.Managers;
 namespace CleanArch.TestingConsoleUi
 {
     class Program
     {
         static void Main(string[] args)
         {
+            StudentManager studentManager = new StudentManager();
+            // Save
+
+            // Update
+            int success = studentManager.Update(1, new CoreLibrary.Models.Student
+            {
+
+            });
+
+            System.Console.WriteLine(success);
 
         }
     }
